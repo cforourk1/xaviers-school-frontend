260e0b27b5f43726b49769aa650a489530cecf32
Crystal <cforourk@umich.edu>
2026-06-27 18:58:24 -0400

    running now wihtout error. going to get in touch with team on next step

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { login } from "../api/users";
 
 export default function Login() {
